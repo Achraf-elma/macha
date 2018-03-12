d4697b581f20f534cd3b9001d84d1982278b3491
Unknown <cyprien_legrand@hotmail.fr>
2018-03-12 11:58:54 +0100

    Mise au propre DAO et ConnectionPostgreSQL

--- src/database/ConnectionPostgreSQL.java
+++ src/database/ConnectionPostgreSQL.java
@@ -1,92 +1,42 @@
+
 package database;
+
 import java.sql.Connection;
-import java.text.SimpleDateFormat;
-import java.sql.*;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class ConnectionPostgreSQL{
-	private String nom;
-	private String mdp;
-	private String dateOfBirth;
-	private String firstname;
-	private String lastname;
-	private String address;
+
+	/**
+	 * URL de connection
+	 */
+	private static String url = "jdbc:postgresql://localhost:5432/macha";
+	/**
+	 * Nom du user
+	 */
+	private static String user = "postgres";
+	/**
+	 * Mot de passe du user
+	 */
+	private static String passwd = "cypcyp";
+	/**
+	 * Objet Connection
+	 */
+	private static Connection connect;
 	
-	public ConnectionPostgreSQL(String userid, String pwd) {
-		try {
-			// Chargement du pilote JDBC
-			Class.forName("org.postgresql.Driver");
-			// URL de connexion
-			String url = "jdbc:postgresql://localhost:5432/macha";
-			// Connexion
-			Connection con = DriverManager.getConnection(url, "postgres", "cypcyp");
-			// Crï¿½ation d'une instruction
-			Statement statement = con.createStatement();
-			// Exï¿½cution d'une requete
-			String query = "SELECT * FROM Users WHERE nickname = '" + userid + "'";
-			ResultSet resultset = statement.executeQuery(query);
-			// Traitement des rï¿½sultats
-			while(resultset.next()) {
-				//System.out.println(resultset.getString(1) + " " + resultset.getString(2));
-				this.nom = resultset.getString(1);
-				this.mdp = resultset.getString(2);
-				this.dateOfBirth = resultset.getString(3);
-				this.firstname = resultset.getString(4);
-				this.lastname = resultset.getString(5);
-				this.address = resultset.getString(6);
+	/**
+	 * Méthode qui va nous retourner notre instance
+	 * et la créer si elle n'existe pas...
+	 * @return
+	 */
+	public static Connection getInstance(){
+		if(connect == null){
+			try {
+				connect = DriverManager.getConnection(url, user, passwd);
+			} catch (SQLException e) {
+				e.printStackTrace();
 			}
-			
-			// fermeture de la connexion
-			con.close();
-		} catch( ClassNotFoundException e) { 
-			System.err.println("Erreur lors du chargement du pilote : " + e);
-		} catch(SQLException sqle) {
-			System.err.print("Erreur SQL : " + sqle);
-		}
-	}
-	
-	//GETTERS & SETTERS
-
-	public String getUserName() {
-		return this.nom;
-	}
-	
-	public String getDateOfBirth() {
-		return dateOfBirth;
-	}
-
-	public void setDateOfBirth(String dateOfBirth) {
-		this.dateOfBirth = dateOfBirth;
-	}
-
-	public String getFirstname() {
-		return firstname;
-	}
-
-	public void setFirstname(String firstname) {
-		this.firstname = firstname;
-	}
-
-	public String getLastname() {
-		return lastname;
-	}
-
-	public void setLastname(String lastname) {
-		this.lastname = lastname;
-	}
-
-	public String getAddress() {
-		return address;
-	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
-	public String getPwd() {
-		return this.mdp;
-	}
-	
-	public static void main(String[] args) {
-		//ConnectionPostgreSQL test = new ConnectionPostgreSQL();
+		}		
+		return connect;	
 	}	
 }
--- src/database/UserDAO.java
+++ src/database/UserDAO.java
@@ -1,22 +1,55 @@
 package database;
 
 import businessLogic.User;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import database.ConnectionPostgreSQL;
 
 public class UserDAO {
 	private User u;
 	
-	private ConnectionPostgreSQL coSql;
+	private String nom;
+	private String mdp;
+	private String dateOfBirth;
+	private String firstname;
+	private String lastname;
+	private String address;
 	
+
+			
 	public void login(String userid, String pwd) { // = createIdByUser()
-		coSql = new ConnectionPostgreSQL(userid, pwd);
-		this.u = new User(coSql.getUserName(), coSql.getPwd());
+		Connection coSql = ConnectionPostgreSQL.getInstance();
+		Statement statement;
+		try {
+			statement = coSql.createStatement();
+			String query = "SELECT * FROM Users WHERE nickname = '" + userid +"'";
+			ResultSet resultset = statement.executeQuery(query);
+			while(resultset.next()) {
+				//System.out.println(resultset.getString(1) + " " + resultset.getString(2));
+				this.nom = resultset.getString(1);
+				this.mdp = resultset.getString(2);
+				this.dateOfBirth = resultset.getString(3);
+				this.firstname = resultset.getString(4);
+				this.lastname = resultset.getString(5);
+				this.address = resultset.getString(6);
+			}
+			
+			this.u = new User(nom, mdp);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		
 		u.login(userid, pwd);
 	}
 	
-	public void getIndexInfos(String userid, String pwd) {
+	/*public void getIndexInfos(String userid, String pwd) {
 		coSql = new ConnectionPostgreSQL(userid, pwd);
 		this.u = new User(coSql.getUserName(), coSql.getPwd(), coSql.getFirstname(), coSql.getLastname(), coSql.getDateOfBirth(), coSql.getAddress());			
-		}
+		}*/
 }
 
 
