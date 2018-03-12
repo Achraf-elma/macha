3df061e00d0e59f557644176a96b220db321ede2
Unknown <cyprien_legrand@hotmail.fr>
2018-03-12 15:12:54 +0100

    Connexion à la BD hébergée par Heroku

--- src/database/ConnectionPostgreSQL.java
+++ src/database/ConnectionPostgreSQL.java
@@ -4,21 +4,22 @@ package database;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class ConnectionPostgreSQL{
 
 	/**
 	 * URL de connection
 	 */
-	private static String url = "jdbc:postgresql://localhost:5432/macha";
+	private static String url = "jdbc:postgresql://ec2-79-125-12-27.eu-west-1.compute.amazonaws.com/de1d4s2ta7acdb?sslmode=require";
 	/**
 	 * Nom du user
 	 */
-	private static String user = "postgres";
+	private static String user = "kycwwiftikpegb";
 	/**
 	 * Mot de passe du user
 	 */
-	private static String passwd = "cypcyp";
+	private static String passwd = "9fa070253aec8d296f0db3310e47ff66904a73e047e214d11675b50a7cb6dd01";
 	/**
 	 * Objet Connection
 	 */
@@ -32,6 +33,8 @@ public class ConnectionPostgreSQL{
 	public static Connection getInstance(){
 		if(connect == null){
 			try {
+				Properties props = new Properties();
+				props.setProperty("ssl","true");
 				connect = DriverManager.getConnection(url, user, passwd);
 			} catch (SQLException e) {
 				e.printStackTrace();
