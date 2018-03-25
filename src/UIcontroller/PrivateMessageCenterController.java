efbfd693a9aa03870e33808badbe8f3350462fb9
Hugo <hugo.fazio@aol.com>
2018-03-26 00:59:16 +0200

    setup

---
+++ src/UIcontroller/PrivateMessageCenterController.java
@@ -0,0 +1,27 @@
+package UIcontroller;
+
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import facade.UserIndexFacade;
+import facade.UserLoginFacade;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.event.ActionEvent;
+
+
+public class PrivateMessageCenterController implements Initializable {
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/facade/PrivateMessageFacade.java
@@ -0,0 +1,18 @@
+package facade;
+
+import database.AbstractDAOFactory;
+import database.PostgresFactory;
+import database.UserDAO;
+import userinterface.UserIndex;
+import businessLogic.User;
+
+public class PrivateMessageFacade {
+
+	/*private AbstractDAOFactory af = new PostgresFactory();
+	private UserDAO ud = af.createUserDAO();
+	
+	public void display(String nick) {
+		ud.display(nick);
+	}*/
+	
+}
