6c06daed1a768374d84f03f26c02497763477958
Unknown <cyprien_legrand@hotmail.fr>
2018-03-30 00:00:15 +0200

    Ajout events

---
+++ src/database/EventDAO.java
@@ -0,0 +1,32 @@
+package database;
+
+import businessLogic.*;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import database.ConnectionPostgreSQL;
+
+public abstract class EventDAO {
+	
+    private Event event;
+    
+    String name;
+    String categName;
+    String location;
+    String date;
+    float price; 
+    int capacity;
+    String description;
+	
+	
+	
+
+			
+	public abstract void createNewEvent(String eventName, String categ, String location, String date, float price, int capacity, String description);
+	
+
+	//public abstract Invitation createInvitation(User senderId, User receiverId, Event event);
+	
+}
---
+++ src/facade/UserEventFormFacade.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package facade;
+
+import database.*;
+
+/**
+ *
+ * @author cyp
+ */
+public class UserEventFormFacade {
+    
+        private AbstractDAOFactory af = new PostgresFactory();
+	private EventDAO ed = af.createEventDAO();
+	private Boolean connected=false;
+    
+        public void createEvent(String name, String category, String location, String date, float price, int capacity, String description){
+            ed.createNewEvent(name, category, location, date, price, capacity, description);
+        }
+}
